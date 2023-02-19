c97f8fb137bc3db50353d7ae98e61628d252a41d
mensh <menshikov1s12006@mail.ru>
2023-02-19 17:31:50 +0500

    stupid things fixed

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
-from os import path
 import shutil
 
 desktop_path = os.path.expanduser("~/Desktop")
 contents = os.listdir(desktop_path)
 
 # 'organize' folder creation
-if not path.exists(f'{desktop_path}/organize'):
+if not os.path.exists(f'{desktop_path}/organize'):
     os.makedirs(f'{desktop_path}/organize')
 organize = os.listdir(f'{desktop_path}/organize')
 
@@ -19,7 +18,7 @@ def split(filepath):
 # folder creation in 'organize' folder
 def folder_creation():
     for i in range(len(contents)):
-        if path.splitext(f'{desktop_path}/{contents[i]}')[1] != '' and not path.exists(
+        if split(f'{desktop_path}/{contents[i]}') != '' and not os.path.exists(
                 desktop_path + '/organize/' + split(f'{desktop_path}/{contents[i]}')) and not split(f'{desktop_path}/{contents[i]}') == '.lnk' and not split(f'{desktop_path}/{contents[i]}') == '.url':
             os.makedirs(desktop_path + '/organize/' + split(f'{desktop_path}/{contents[i]}'))
 
@@ -27,7 +26,7 @@ def folder_creation():
 # moving the files in its extension folder
 def file_move():
     for j in organize:
-        if j != '' and path.isdir(f'{desktop_path}/organize/{j}'):
+        if j != '' and os.path.isdir(f'{desktop_path}/organize/{j}'):
             for i in contents:
                 if split(i) == j and i != '':
                     shutil.move(f'{desktop_path}/{i}', f'{desktop_path}/organize/{j}')
